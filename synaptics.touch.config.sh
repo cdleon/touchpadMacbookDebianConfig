794ce0c22f4843d7b0bfcbfff87ee3da52aea958
cdDebMacbook <cdleon15@yahoo.com>
2014-06-10 17:44:29 -0400

    second commit

--- synaptics.touch.config.sh
+++ synaptics.touch.config.sh
@@ -7,4 +7,6 @@ synclient FingerHigh=1
 synclient TapButton1=1
 synclient TapButton2=2
 synclient TapButton3=3
+synclient TapAndDragGesture=0
 synclient HorizTwoFingerScroll=1
+synclient PalmDetect=1

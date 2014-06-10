33d8de9ca380bd634cd9cc593dd7fa635c593be8
cdDebMacbook <cdleon15@yahoo.com>
2014-06-10 17:34:49 -0400

    first commit

---
+++ synaptics.touch.config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Synaptics config for debian on macbook 4,1
+
+synclient FingerLow=1
+synclient FingerHigh=1
+synclient TapButton1=1
+synclient TapButton2=2
+synclient TapButton3=3
+synclient HorizTwoFingerScroll=1
